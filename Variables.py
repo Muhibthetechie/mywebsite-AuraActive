85506d4230a5839db21d8c5056ea39165c955726
Muhibthetechie <muhibsaquib@gmail.com>
2024-10-24 19:26:05 +0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,7 @@
+x = 5
+y = "Jhon"
+print(x)
+print(y)
+
+name = input("Enter your name:")
+print("\nHi",name,"\nwelcome")
---
+++ print.py
@@ -0,0 +1,1 @@
+print("My name is Muhib Hussain Saqib. I study in grade7. My favourite sport is cricket. I also like to play video games. I am a web doveloper.")
