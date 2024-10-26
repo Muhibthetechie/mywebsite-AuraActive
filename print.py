3c64873ecadba8894e60a64fc5cbf0b5081127b2
Muhibthetechie <muhibsaquib@gmail.com>
2024-10-26 20:53:56 +0500

    Add files via upload

--- print.py
+++ print.py
@@ -1,1 +1,29 @@
 print("My name is Muhib Hussain Saqib. I study in grade7. My favourite sport is cricket. I also like to play video games. I am a web doveloper.")
+a = 5000
+e = float(a)
+print(type (a))
+print(type (e))
+
+b = 34.748
+j = str(b)
+print(type (b))
+print(type(j))
+
+
+c= "Muhib"
+#q = int(c)
+print(type (c))
+#print(type(q))
+
+
+d= True
+w = int(d)
+print(type(d))
+print(type (w))
+
+
+text=str(input("Enter a string: "))
+revText = text[::-1]
+text= revText
+print("reverse of give string is:")
+print(text)
